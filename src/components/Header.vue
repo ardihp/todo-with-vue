93f4354fdf818ddc5d514ca38ec079d23840d30c
ardihp <ardihp8@gmail.com>
2022-10-21 14:41:11 +0700

    style: fix header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center py-8 px-10 shadow-md shadow-slate-300 border-b-2 border-gray-400">
+  <div class="flex justify-center py-8 px-10">
     <div class="max-w-screen-xl mx-auto w-full flex justify-center">
       <div class="text-xl font-bold flex gap-1 items-end text-gray-500">
         Todo <ChecklistIcon size="36" /> Check
